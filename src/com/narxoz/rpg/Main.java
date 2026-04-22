a12e38a1ac737075ce567cbc5cc18dbe1d251b2a
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-04-22 21:43:53 +0500

    feat: print final TowerRunResult to complete demo

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -3,6 +3,8 @@ package com.narxoz.rpg;
 import com.narxoz.rpg.combatant.Hero;
 import com.narxoz.rpg.floor.*;
 import com.narxoz.rpg.state.PoisonedState;
+import com.narxoz.rpg.tower.TowerRunResult;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,12 +24,29 @@ public class Main {
                 new BossFloor()
         );
 
+        System.out.println("Ascending The Haunted Tower...\n");
+
         int floorsCleared = 0;
         for (TowerFloor floor : tower) {
-            if (party.stream().noneMatch(Hero::isAlive)) break;
+            if (party.stream().noneMatch(Hero::isAlive)) {
+                System.out.println("\nAll heroes have fallen.");
+                break;
+            }
             FloorResult result = floor.explore(party);
-            if (result.isCleared()) floorsCleared++;
-            else break;
+            if (result.isCleared()) {
+                floorsCleared++;
+            } else {
+                break;
+            }
         }
+
+        long survivors = party.stream().filter(Hero::isAlive).count();
+        boolean reachedTop = (floorsCleared == tower.size());
+        TowerRunResult runResult = new TowerRunResult(floorsCleared, (int) survivors, reachedTop);
+
+        System.out.println("\n=== TOWER RUN RESULT ===");
+        System.out.println("Floors Cleared: " + runResult.getFloorsCleared() + "/" + tower.size());
+        System.out.println("Heroes Surviving: " + runResult.getHeroesSurviving());
+        System.out.println("Tower Defeated: " + (runResult.isReachedTop() ? "YES" : "NO"));
     }
 }
