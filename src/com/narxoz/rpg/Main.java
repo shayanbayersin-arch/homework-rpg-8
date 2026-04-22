986e6ad842d502b02de756a91d98ed2fb2d0bb85
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-04-22 21:41:04 +0500

    feat: setup initial party and floor list in Main

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -2,18 +2,32 @@ package com.narxoz.rpg;
 
 import com.narxoz.rpg.combatant.Hero;
 import com.narxoz.rpg.floor.*;
+import com.narxoz.rpg.state.PoisonedState;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Hero> party = new ArrayList<>();
-        List<TowerFloor> tower = new ArrayList<>();
+        Hero arthur = new Hero("Arthur", 100, 20, 5);
+        Hero lancelot = new Hero("Lancelot", 80, 25, 3);
+        lancelot.setState(new PoisonedState());
 
+        List<Hero> party = new ArrayList<>(Arrays.asList(arthur, lancelot));
+
+        List<TowerFloor> tower = Arrays.asList(
+                new CombatFloor(),
+                new TrapFloor(),
+                new RestFloor(),
+                new BossFloor()
+        );
+
+        int floorsCleared = 0;
         for (TowerFloor floor : tower) {
             if (party.stream().noneMatch(Hero::isAlive)) break;
             FloorResult result = floor.explore(party);
-            if (!result.isCleared()) break;
+            if (result.isCleared()) floorsCleared++;
+            else break;
         }
     }
 }
